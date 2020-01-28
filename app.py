5d9ce2d89d3bfa1040439b38bbd2c9359eea9984
csmfindling <charles.findling@gmail.com>
2020-01-28 15:13:21 +0100

    updated requirements

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ CORS(app)
 with app.app_context():
     install_models()
     import routes
-
+ 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", debug=True, port=port)
