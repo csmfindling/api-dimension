a4c0bcb05280fe62bd62423f99ce9f4c577d2883
csmfindling <charles.findling@gmail.com>
2020-01-28 15:59:31 +0100

    test

--- app.py
+++ app.py
@@ -25,5 +25,4 @@ def mytest():
     return jsonify(result), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", debug=True)
